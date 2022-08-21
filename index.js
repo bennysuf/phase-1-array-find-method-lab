6cea06fb277bb5f58a79deb3f78aad7acd2ca976
Benny Sufrin <bennysuf@gmail.com>
2022-08-21 14:14:44 -0400

    commit tree c6622a4fec6b1b26f87d0caa9ccb360ccd5409ef

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// code your solution here
+
+//     function superbowlWin(element) {
+//      const result = element.find(isWin => isWin.result === "W")
+//      if (result){
+//       return result.year;
+//      }  
+// }
+
+function superbowlWin(array) {
+  const winningSeason = array.find(function (blah) {
+    return blah.result === "W";
+  })
+  if (winningSeason) {
+    return winningSeason.year;
+  }
+}
